9d3d5b27219ffe1dd96499a2e9bd4896731e2ab3
mandolyte <cecil.new@gmail.com>
2023-03-29 22:15:32 -0400

    fixed exports

---
+++ src/core/index.js
@@ -0,0 +1,4 @@
+export {checkMergeDefaultIntoUserBranch} from './checkMergeDefaultIntoUserBranch.js'
+export {checkMergeUserIntoDefaultBranch} from './checkMergeUserIntoDefaultBranch.js'
+export {mergeDefaultIntoUserBranch} from './mergeDefaultIntoUserBranch.js'
+export {mergeUserIntoDefaultBranch} from './mergeUserIntoDefaultBranch.js'
