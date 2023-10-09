5485316cb3e0d635bf069aa64ee266dff47b18f5
Noah Harvey <noah.harvey247@gmail.com>
2023-10-09 11:31:42 -0400

    rewrites checkMergeDefaultIntoUserBranch to use promise syntax
    the rewrite uses a more declarative style to make the return
    values of the function more obvious.

--- src/core/checkMergeDefaultIntoUserBranch.js
+++ src/core/checkMergeDefaultIntoUserBranch.js
@@ -1,37 +1,42 @@
 import { getPrJsonByUserBranch, checkFilenameUpdateable } from './common'
 
-export async function checkMergeDefaultIntoUserBranch({
+export const checkMergeDefaultIntoUserBranch = ({
   server, owner, repo, userBranch, prDescription, tokenid, filename,
-}) {
-  let returnObject = {
-    mergeNeeded: false,
-    conflict: false, 
-    error: false,
-    message: "",
-    pullRequest: "",
-  };
-  let prJson = {}
-  try {
-    prJson = await getPrJsonByUserBranch( {server, owner, repo, userBranch, prBody: prDescription, tokenid} )
-  } catch (e) {
-    returnObject.error = true
-    returnObject.message = e.message
-    return returnObject
-  }
-  const mergeable = prJson.mergeable
-  const headSha = prJson.head.sha
-  const baseSha = prJson.base.sha
-  const mergeBase = prJson.merge_base
-  const pullRequest = prJson.url
-  returnObject.conflict = ! mergeable
-  returnObject.mergeNeeded = mergeable && (headSha !== baseSha && baseSha !== mergeBase)
-  returnObject.pullRequest = pullRequest
-  if ( mergeable && filename ) {
-    // narrow the context for mergeability to this single file, ignoring any others
-    // Do note: the boolean for mergeable is in terms of the entire branch.
-    // Therefore while the conflict may exist in a different file, out of an abundance
-    // of caution we will not test for the provided file when conflicts exist anywhere
-    returnObject.mergeNeeded = await checkFilenameUpdateable({server, owner, repo, prJson, filename})
-  }
-  return returnObject
-}
+}) => {
+  getPrJsonByUserBranch( {server, owner, repo, userBranch, prBody: prDescription, tokenid} )
+  .then(({mergeable, head, base, merge_base, url}) => checkFilenameUpdateable({server, owner, repo, prJson, filename})
+  .then(fileNameUpdatable => 
+      ({
+        // narrow the context for mergeability to this single file, ignoring any others
+        // Do note: the boolean for mergeable is in terms of the entire branch.
+        // Therefore while the conflict may exist in a different file, out of an abundance
+        // of caution we will not test for the provided file when conflicts exist anywhere
+        mergeNeeded: mergeable && 
+          (filename && fileNameUpdatable 
+          || (head.sha !== base.sha && base.sha !== merge_base),
+          ),
+        conflict: !mergeable, 
+        error: false,
+        message: "",
+        pullRequest: url
+      })   
+    ))
+  .catch(e =>
+    //TODO: it would be safer to return  a sum type of the happy path object 
+    //and { error : true, message : string} this prevents users of this API
+    //from using invalid states. E.g mergeNeeded is false when, technically,
+    //we don't know whether mergeNeeded is true or false because an error occured. 
+    //undefined/null could be used, however that creates more bugs (users have to
+    //liter their code with null/undefined checks).
+    ({
+        // narrow the context for mergeability to this single file, ignoring any others
+        // Do note: the boolean for mergeable is in terms of the entire branch.
+        // Therefore while the conflict may exist in a different file, out of an abundance
+        // of caution we will not test for the provided file when conflicts exist anywhere
+        mergeNeeded: false,
+        conflict: false, 
+        error: true,
+        message: e.message,
+        pullRequest: ""
+    })   
+
