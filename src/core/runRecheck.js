5af1911d359b8f4f714d7dd4499aad8fd8c62075
Noah Harvey <noah.harvey247@gmail.com>
2023-11-16 12:34:02 -0500

    adds documentation to runCheck

--- src/core/runRecheck.js
+++ src/core/runRecheck.js
@@ -1,5 +1,27 @@
 /**
-@todo document the params here
+@typedef {number} Natural
+A positive integer greater than or equal to zero
+
+@typedef {Natural} RunCount
+
+@callback Predicate
+@param {any} the unwrapped value returned by a Computation.
+@returns {boolean}
+
+@callback Computation
+@param {RunCount} the number of times the computation has been _previously_ called
+@returns {Promise<any>} literally anything 
+*/
+
+/**
+@function
+@description Calls the given computation repeatedly as long as the
+predicate is true or the max number or retries has been reached.
+@param {object} arg
+@param {RunCount} arg.maxRecheckCount
+@param {Predicate} arg.predicate
+@param {Computation} arg.computation
+@returns {Promise<any>} the result of calling computation
 */
 export const runRecheckWhen = async ({maxRecheckCount, predicate, computation}) => {
   let runCount = 0;
@@ -8,22 +30,24 @@ export const runRecheckWhen = async ({maxRecheckCount, predicate, computation}) 
     runCount++;
   } while (runCount <= maxRecheckCount && predicate(result))
 
-  return {result, runCount}
+  return result
 }
 
 /**
 @todo maybe we should introduce jest for the lib here?
 */
 const test = () => {  
+  const saveCount = (n) => Promise.resolve(n)
+
   //running with a predicate that is always true should produce a count o
   Promise.all
-  ([ runRecheckWhen({maxRecheckCount: 2, predicate : () => false, computation : () => Promise.resolve()})
-    .then(({runCount}) => console.assert(runCount == 1, `recheck min failed: ${runCount} should be 1`))
+  ([ runRecheckWhen({maxRecheckCount: 2, predicate : () => false, computation : saveCount})
+    .then(runCount => console.assert(runCount == 0, `recheck min failed: ${runCount} should be 1`))
 
-  , runRecheckWhen({maxRecheckCount : 2, predicate : () => true, computation : () => Promise.resolve()})
-    .then(({runCount}) => console.assert(runCount == 3, `recheck max failed: ${runCount} should be 2`))
+  , runRecheckWhen({maxRecheckCount : 2, predicate : () => true, computation : saveCount })
+    .then(runCount => console.assert(runCount == 2, `recheck max failed: ${runCount} should be 2`))
 
-  , runRecheckWhen({maxRecheckCount : 3, predicate : (n) => n === 0, computation : (n) => Promise.resolve(n)})
-    .then(({runCount}) => console.assert(runCount == 2, `recheck failed: ${runCount} should be 1`))
+  , runRecheckWhen({maxRecheckCount : 3, predicate : (n) => n === 0, computation : saveCount })
+    .then(runCount => console.assert(runCount == 1, `recheck failed: ${runCount} should be 1`))
   ])
-} 
+}  
